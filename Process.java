ea57bd9489883cba550de1c1feb873b87a486094
arjunsridhar1234 <48868607+arjunsridhar1234@users.noreply.github.com>
2022-02-19 23:10:47 -0600

    Update Process.java

--- Process.java
+++ Process.java
@@ -10,9 +10,9 @@ public class Process {
 	private int processId; // unique process id
 	private boolean status; // status - active or running
 	private int maxId; // max id process has seen
-	private Set<Integer> neighbors; // set of neighbors for process
-	private Set<Integer> children; // children of process for termination detection
-	private Set<Integer> others; // non-children
+	private List<Integer> neighbors; // set of neighbors for process
+	private List<Integer> children; // children of process for termination detection
+	private List<Integer> others; // non-children
 	private int parent; // parent of process
 	private boolean termination; // whether to terminate or not
 	private BlockingQueue<Message> inbox; // 
@@ -24,6 +24,73 @@ public class Process {
 		this.status = true; // active
 		this.maxId = processId;
 		termination = false;
+		this.parent = 0;
+		children = new ArrayList<Integer>();
 		
 	}
+
+	public int getProcessId() {
+		return processId;
+	}
+
+	public void setProcessId(int processId) {
+		this.processId = processId;
+	}
+
+	public boolean isStatus() {
+		return status;
+	}
+
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+
+	public int getMaxId() {
+		return maxId;
+	}
+
+	public void setMaxId(int maxId) {
+		this.maxId = maxId;
+	}
+
+	public List<Integer> getNeighbors() {
+		return neighbors;
+	}
+
+	public void setNeighbors(List<Integer> neighbors) {
+		this.neighbors = neighbors;
+	}
+
+	public List<Integer> getChildren() {
+		return children;
+	}
+
+	public void setChildren(List<Integer> children) {
+		this.children = children;
+	}
+
+	public List<Integer> getOthers() {
+		return others;
+	}
+
+	public void setOthers(List<Integer> others) {
+		this.others = others;
+	}
+
+	public int getParent() {
+		return parent;
+	}
+
+	public void setParent(int parent) {
+		this.parent = parent;
+	}
+
+	public boolean isTermination() {
+		return termination;
+	}
+
+	public void setTermination(boolean termination) {
+		this.termination = termination;
+	}
 }
+
