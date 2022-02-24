0bcfd41edb6dac0316e91e71406834e7b5e0d2ad
arjunsridhar1234 <48868607+arjunsridhar1234@users.noreply.github.com>
2022-02-24 00:52:51 -0600

    Added MainThread with some small changes to other stuff

---
+++ src/WorkerProcess.java
@@ -0,0 +1,244 @@
+package src;
+
+//Importing utility classes
+import java.util.*;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class WorkerProcess extends Process implements Runnable{
+	private int processId; // unique process id
+	private boolean status; // status - active or running
+	private int maxId; // max id process has seen
+	private boolean isLeader;
+	private boolean isReadyToTerminate = false;
+	private boolean isLeaderFound = false;
+
+	private HashMap<Integer, Process> neighbors; // set of neighbors for process
+	private Set<Integer> children; // children of process for termination detection
+	private Set<Integer> others; // non-children
+	private Set<Integer> terminatedNeighbor; // non-children
+
+	private int parent; // parent of process
+	private Process master; // reference of master process
+	private CyclicBarrier barrier; // sync message processing, sending, etc.
+
+	// temporary space, empty after each round
+	private Set<Integer> receivedREJsFrom = new HashSet<>();
+	private Set<Integer> receivedACKsFrom = new HashSet<>();
+	
+	public WorkerProcess(int processId) {
+		this.processId = processId;
+		this.inbox = new LinkedBlockingQueue<Message>();
+		
+	}
+	
+	public void setBarrier(CyclicBarrier barrier) {
+		this.barrier = barrier;
+	}
+	
+	public void setWorkerProcess(Process master, HashMap<Integer, Process> neighbors, CyclicBarrier barrier) {
+		this.status = true; // active
+		this.maxId = processId;
+		this.parent = -1;
+		this.master = master;
+		this.isLeader = false;
+		this.inbox = new LinkedBlockingQueue<Message>();
+		this.barrier = barrier;
+
+		this.neighbors = new HashMap<>(neighbors);
+		this.children = new HashSet<>();
+		this.others = new HashSet<>(this.neighbors.keySet());
+		this.terminatedNeighbor = new HashSet<>();
+	}
+
+	public int getProcessId() {
+		return processId;
+	}
+
+	private void kill() {
+		this.status = false;
+	}
+
+	private void setMaxId(int maxId) {
+		this.maxId = maxId;
+	}
+
+	private void waitToStartRound() throws InterruptedException {
+		boolean flag = false;
+		while(!flag){
+			Message msg = this.inbox.take();
+			if(msg.getMessageType().equals(Type.BGN)){
+				// start current round
+				//System.out.println(msg.getSenderId() + " " + this.processId);
+				flag = true;
+			} else if(msg.getMessageType().equals(Type.FIN)){
+				// start self destruction
+				this.status = false;
+				flag = true;
+			} else {
+				//this.inbox.offer(msg);
+			}
+		}
+	}
+
+	private void compareId() throws InterruptedException {
+		// handle message bc xpl and tmn would be handled at the same time
+		int numOfMessages = this.inbox.size();
+		for(int i=0;i<numOfMessages; i++) {
+			Message msg = this.inbox.take();
+			switch (msg.getMessageType()) {
+				case XPL:
+					// this time the info id is
+					if (msg.getInfoId() > this.maxId) {
+						// update maxID
+						this.maxId = msg.getInfoId();
+						this.parent = msg.getSenderId();
+					}
+					break;
+				case TMN:
+					// this time the info id is
+					this.terminatedNeighbor.add(msg.getSenderId());
+				default:
+					this.inbox.offer(msg);
+					break;
+			}
+		}
+	}
+
+	private void sendResponse(){
+		for(Map.Entry<Integer, Process> pair : neighbors.entrySet()){
+			if(pair.getKey().equals(this.parent)){
+				Message msg = new Message(this.processId, Type.ACK);
+				pair.getValue().putInMessage(msg);
+			} else {
+				Message msg = new Message(this.processId, Type.REJ);
+				pair.getValue().putInMessage(msg);
+			}
+		}
+	}
+
+	private void collectResponse() throws InterruptedException {
+		int numOfMessages = this.inbox.size();
+		for(int i=0;i<numOfMessages; i++){
+			Message msg = this.inbox.take();
+			switch (msg.getMessageType()){
+				case ACK:
+					// this time the info id is
+					this.children.add(msg.getSenderId());
+					this.others.remove(msg.getSenderId());
+					this.receivedACKsFrom.remove(msg.getSenderId());
+					break;
+				case REJ:
+					this.others.add(msg.getSenderId());
+					this.children.remove(msg.getSenderId());
+					this.receivedREJsFrom.remove(msg.getSenderId());
+					this.isLeader = false;
+					break;
+				default:
+					this.inbox.offer(msg);
+					break;
+			}
+		}
+	}
+
+	private void explore(){
+		Message msg = new Message(this.processId, this.maxId, Type.XPL);
+		this.broadcast(this.neighbors.values(), msg);
+	}
+
+	private void checkLeader(){
+		if(this.receivedACKsFrom.size() == this.children.size()){
+			this.isLeader = true;
+		}
+	}
+
+	private void checkTerminate(){
+		// check if current thread should terminate
+		boolean allChildrenTerminated = false;
+		boolean receivedNACKFromOthers = false;
+
+		// check if a process has received notifications of completion from all its children
+		Set<Integer> temp = new HashSet<>(this.children);
+		temp.removeAll(this.terminatedNeighbor);
+		if (temp.isEmpty()) {
+			// all children have terminated
+			allChildrenTerminated = true;
+		}
+
+		// check if a process has received REJs from other neighbors
+		// the process shouldn't expect a REJ from the parent to terminate, it may have happened in an earlier round
+		temp =  new HashSet<>(this.receivedREJsFrom);
+		temp.removeAll(others);
+		if (temp.isEmpty() && !others.isEmpty()) {
+			receivedNACKFromOthers = true;
+		}
+
+		if (this.parent != -1 && allChildrenTerminated && receivedNACKFromOthers) {
+			this.isReadyToTerminate = true;
+		} else isReadyToTerminate = this.parent == -1 && allChildrenTerminated && isLeader;
+	}
+
+	@Override
+	public void run(){
+		while(this.status){
+			try {
+				//System.out.println("WP " + this.processId + this.status);
+				waitToStartRound();
+				//if(!this.status){
+					//System.out.println("Done: " + this.processId);
+					//break;
+				//}
+				
+				explore();
+				this.barrier.await();
+				sendResponse();
+				compareId();
+				
+				this.barrier.await();
+				collectResponse();
+
+				this.receivedREJsFrom.clear();
+				this.receivedACKsFrom.clear();
+
+				checkLeader();
+				checkTerminate();
+				
+				if(this.isReadyToTerminate){
+					//System.out.println("terminate: " + this.processId);
+					Message msg = new Message(this.processId, this.parent, Type.TMN);
+					this.master.putInMessage(msg);
+					broadcast(this.neighbors.values(), msg);
+					
+				} else{
+					if(this.isLeader) {
+						//System.out.println("Lead: " + this.processId);
+						Message msg = new Message(this.processId, this.parent, Type.LDB);
+						this.master.putInMessage(msg);
+						broadcast(this.neighbors.values(), msg);
+						break;
+					}
+					else {
+						//System.out.println("end: " + this.processId);
+						Message msg = new Message(this.processId, Type.END);
+						this.master.putInMessage(msg);
+					}
+				}
+				
+			} catch (InterruptedException | BrokenBarrierException e) {
+				e.printStackTrace();
+			}
+		}
+		//System.out.println(this.status);
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Process{" +
+				"uid=" + this.processId +
+				'}';
+	}
+}
+
